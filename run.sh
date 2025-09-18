c28abdb33e6edcfe98ebe0d564c9914ee2786029
adrian <muhamadadrian42994@gmail.com>
2025-09-18 13:37:20 +0700

    first commit

--- run.sh
+++ run.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 
+clear
+
 banner() {
-    clear
     local user_name=$(whoami)
     local time_now=$(date +"%Y-%m-%d %H:%M:%S")
-    
-    echo -e "\e[1;92m====================================================\e[0m"
-    echo -e "\e[1;96m"
-    echo "██╗   ██╗ █████╗ ███╗   ██╗"
-    echo "██║   ██║██╔══██╗████╗  ██║"
-    echo "██║   ██║███████║██╔██╗ ██║"
-    echo "╚██╗ ██╔╝██╔══██║██║╚██╗██║"
-    echo " ╚████╔╝ ██║  ██║██║ ╚████║"
-    echo "  ╚═══╝  ╚═╝  ╚═╝╚═╝  ╚═══╝"
-    echo -e "\e[0m"
-    echo -e "\e[1;93m                  ✦✦✦   YAN   ✦✦✦\e[0m"
-    echo -e "\e[1;95m                       Adrian\e[0m"
-    echo -e "\e[1;92m====================================================\e[0m"
+    local ip_pub=$(curl -s https://api.ipify.org)
+
+    echo -e "\e[1;34m╔════════════════════════════════════════╗\e[0m"
+    echo -e "\e[1;34m║           ( T ) ( O ) ( O ) ( L ) ( S ) V5          ║\e[0m"
+    echo -e "\e[1;34m╚════════════════════════════════════════╝\e[0m"
     echo ""
-    echo -e "\e[1;36mUser       : \e[0m$user_name"
-    echo -e "\e[1;36mWaktu      : \e[0m$time_now"
-    echo -e "\e[1;36mPilih fitur yang ingin dijalankan:\e[0m"
+    echo -e "\e[1;32mAuthor  : \e[0mLucifer"
+    echo -e "\e[1;32mGithub  : \e[0mgithub.com/lucifer"
+    echo -e "\e[1;32mWaktu   : \e[0m$time_now"
+    echo -e "\e[1;32mUser    : \e[0m$user_name"
+    echo -e "\e[1;32mIP Pub  : \e[0m$ip_pub"
     echo ""
+    echo -e "\e[1;31mSILAHKAN PILIH TOOLS DIBAWAH\e[0m"
     echo -e "\e[1;33m[1] Kamera (CobaHak)\e[0m"
     echo -e "\e[1;33m[2] Lacak Lokasi (Fake)\e[0m"
+    echo -e "\e[1;33m[3] Lacak IP (IP Repo)\e[0m"
     echo ""
+}
 
-    read -p "Masukkan pilihan [1/2]: " option
+menu() {
+    read -p "Masukkan pilihan [1/2/3]: " option
     option="${option:-1}"
 
-    # Konfirmasi sebelum menjalankan
     read -p "Kamu yakin ingin menjalankan pilihan ini? (y/n): " confirm
     if [[ $confirm != [yY] ]]; then
         echo -e "\e[1;91m[!] Dibatalkan oleh user. Keluar...\e[0m"
@@ -40,14 +38,20 @@ banner() {
         1)
             echo -e "\e[1;92m[*] Mengunduh & menjalankan CobaHak (Kamera)...\e[0m"
             git clone https://github.com/adrian020w/CobaHak.git
-            cd CobaHak
+            cd CobaHak || exit
             bash camphish.sh
             ;;
         2)
             echo -e "\e[1;92m[*] Mengunduh & menjalankan Fake (Lacak Lokasi)...\e[0m"
             git clone https://github.com/umbrahabreh-arch/fake.git
-            cd fake
+            cd fake || exit
             bash camphish.sh
+            ;;
+        3)
+            echo -e "\e[1;92m[*] Mengunduh & menjalankan IP Tracker...\e[0m"
+            git clone https://github.com/adrian020w/ip.git
+            cd ip || exit
+            bash cam.sh
             ;;
         *)
             echo -e "\e[1;91m[!] Pilihan tidak valid. Keluar...\e[0m"
@@ -56,5 +60,6 @@ banner() {
     esac
 }
 
-# Jalankan banner
+# Jalankan banner dan menu
 banner
+menu
